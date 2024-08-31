c8745020eeede67ad1cea2e79868da3e9b404931
Дмитрий <dima.voron13666@gmail.com>
2024-08-31 19:00:38 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> numbers = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        List<String> words = new ArrayList<>(List.of("cat", "dog", "dog", "fish", "bird", "cat", "horse", "mouse", "horse", "human"));
+
+        System.out.println("Задание 1");
+        for (int i : numbers) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+        System.out.println();
+
+        System.out.println("Задание 2");
+        Collections.sort(numbers);
+        int numberMin = numbers.get(0);
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numberMin < numbers.get(i) && numbers.get(i) % 2 == 0) {
+                numberMin = numbers.get(i);
+                System.out.print(numbers.get(i) + " ");
+            }
+        }
+
+        System.out.println();
+
+        System.out.println("Задание 3");
+        Set<String> wordWithoutRepeat = new HashSet<>(words);
+        System.out.println("\n" + words);
+        System.out.println(wordWithoutRepeat);
+        System.out.println();
+
+        System.out.println("Задание 4");
+        System.out.println();
+        for (String temp : wordWithoutRepeat) {
+            int count = 0;
+            for (String temp2 : words) {
+                if (temp.equals(temp2)) {
+                    count++;
+                }
+            }
+            System.out.println(temp + ": " + count);
+        }
     }
 }
